b1d0976bd2417c70e4107921d2b151c94fd2ff33
Hamza Mohamed <Hamzafowzi1@gmail.com>
2021-06-21 02:57:22 -0500

    Added more code to the function.js file

--- function.js
+++ function.js
@@ -54,3 +54,49 @@ const numOfMonitors = monitorCount(5, 4);
 
 //login the variable to the console
 console.log(numOfMonitors)
+
+//Helper function
+function monitorCount(rows, columns) {
+  return rows * columns;
+}
+
+
+function costOfMonitors(rows, columns){
+  return monitorCount (rows, columns) * 200
+}
+
+const totalCost = costOfMonitors(5, 4);
+
+console.log(totalCost)
+
+//Function expression
+const plantNeedsWater = function(day){
+if (day === "Wednesday"){
+  return true;
+}else {
+  return false;
+}
+}
+
+console.log(plantNeedsWater("Tuesday"));
+
+
+//Arrow function
+
+// const plantNeedsWater = function(day) {
+//   if (day === 'Wednesday') {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// };
+
+const plantNeedssWater = day =>{
+
+}
+
+const plantNeedsssWater = (day) => {
+  return day === 'Wednesday' ? true : false;
+};
+
+const plantNeedWater = day => day === 'Wednesday' ? true : false;
